82f0fe5100dbb1b82164faab09f5ac1cd3e17bfc
Craig Burke <(none)>
2016-06-13 10:16:04 -0400

    Code syntax highlighting

--- src/assets/javascripts/main.js
+++ src/assets/javascripts/main.js
@@ -1,11 +1,15 @@
 //= require /bespoke/bespoke
 //= require_full_tree /bespoke/plugins/
 //= require_full_tree /bespoke/themes/
+//= require highlightjs/highlight.pack
 
 bespoke.from("#presentation", [
     bespoke.themes.nebula(),
     bespoke.plugins.keys(),
     bespoke.plugins.hash(),
     bespoke.plugins.backdrop(),
-    bespoke.plugins.progress()
+    bespoke.plugins.bullets(),
+    bespoke.plugins.backdrop()
 ]);
+
+hljs.initHighlightingOnLoad();
